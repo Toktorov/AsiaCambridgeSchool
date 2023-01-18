0379235d4f1887cc2933c819ddf503d92dc62e5b
Toktorov <ktoktorov2005@gmail.com>
2023-01-18 12:56:53 +0600

    asia postgres db

--- asia_cambridge/settings.py
+++ asia_cambridge/settings.py
@@ -106,8 +106,12 @@ WSGI_APPLICATION = 'asia_cambridge.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get("NAME"),
+        'USER': os.environ.get("USER"),
+        'PASSWORD': os.environ.get("PASSWORD"),
+        'HOST': os.environ.get("HOST"),
+        'PORT': os.environ.get("PORT"),
     }
 }
 
