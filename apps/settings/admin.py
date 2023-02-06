92e8d485d654def2dfb9ba150394077ff2e8af6f
Toktorov <ktoktorov2005@gmail.com>
2023-02-06 18:52:27 +0600

    Accreditation admin

--- apps/settings/admin.py
+++ apps/settings/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from apps.settings.models import Setting, Progress, AboutSchool, SchoolHistory, News, Class
+from apps.settings.models import Setting, Progress, AboutSchool, SchoolHistory, News, Class, Accreditation
 
 # Register your models here.
 admin.site.register(Setting)
@@ -8,3 +8,4 @@ admin.site.register(AboutSchool)
 admin.site.register(SchoolHistory)
 admin.site.register(News)
 admin.site.register(Class)
+admin.site.register(Accreditation)
